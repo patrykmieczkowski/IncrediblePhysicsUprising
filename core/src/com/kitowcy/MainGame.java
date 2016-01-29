ec507740a3fac1b0f21cb608e5849678a5d699c4
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-01-29 22:06:36 +0100

    added logs

--- core/src/com/kitowcy/MainGame.java
+++ core/src/com/kitowcy/MainGame.java
@@ -4,24 +4,41 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainGame extends ApplicationAdapter {
+
+	public static final String TAG = MainGame.class.getSimpleName();
+
 	SpriteBatch batch;
 	Texture img;
+	Sprite spirte;
 	
 	@Override
 	public void create () {
+		Gdx.app.log(TAG, "create()");
 		batch = new SpriteBatch();
 		img = new Texture("backein.jpg");
+		spirte = new Sprite(img);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 1, 0);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		spirte.draw(batch);
 		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		Gdx.app.log(TAG, "dispose()");
+		batch.dispose();
+		img.dispose();
 	}
 }
