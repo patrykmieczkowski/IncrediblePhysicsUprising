c4b63736a808efdc02da4a06bd52cb5fcacfced5
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-01-29 21:26:54 +0100

    Initial commit

---
+++ android/src/com/kitowcy/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kitowcy;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kitowcy.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
