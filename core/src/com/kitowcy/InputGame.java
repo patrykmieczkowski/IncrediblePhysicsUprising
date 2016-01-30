504acfddb6b3bb8081a1837acf0802ae7ca53140
VaccumMan <lukmar93@o2.pl>
2016-01-30 11:04:04 +0100

    accelerometer cat 2

--- core/src/com/kitowcy/InputGame.java
+++ core/src/com/kitowcy/InputGame.java
@@ -7,11 +7,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.math.Vector2;
 
 public class InputGame extends ApplicationAdapter implements InputProcessor {
-
+    float w;
+    float h;
     public static final String TAG = InputGame.class.getSimpleName();
     private SpriteBatch batch;
     private Texture texture;
@@ -20,53 +19,66 @@ public class InputGame extends ApplicationAdapter implements InputProcessor {
     @Override
     public void create() {
         Gdx.app.log(TAG, "create()");
-        GestureDetector gd = new GestureDetector(new GestureDetector.GestureListener() {
-            @Override
-            public boolean touchDown(float x, float y, int pointer, int button) {
-                return false;
-            }
 
-            @Override
-            public boolean tap(float x, float y, int count, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean longPress(float x, float y) {
-                return false;
-            }
-
-            @Override
-            public boolean fling(float velocityX, float velocityY, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean pan(float x, float y, float deltaX, float deltaY) {
-                return false;
-            }
-
-            @Override
-            public boolean panStop(float x, float y, int pointer, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean zoom(float initialDistance, float distance) {
-                return false;
-            }
-
-            @Override
-            public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-                return false;
-            }
-        });
-        Gdx.input.setInputProcessor(this);
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
+//        InputMultiplexer im = new InputMultiplexer();
+//        GestureDetector gd = new GestureDetector(new GestureDetector.GestureListener() {
+//            @Override
+//            public boolean touchDown(float x, float y, int pointer, int button) {
+//                Gdx.app.log(TAG, "touchDown " + pointer);
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean tap(float x, float y, int count, int button) {
+//                Gdx.app.log(TAG, "tap " + count + " times");
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean longPress(float x, float y) {
+//                Gdx.app.log(TAG, "longPress");
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean fling(float velocityX, float velocityY, int button) {
+//                Gdx.app.log(TAG, "fling");
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean pan(float x, float y, float deltaX, float deltaY) {
+//                Gdx.app.log(TAG, "pan");
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean panStop(float x, float y, int pointer, int button) {
+//                Gdx.app.log(TAG, "panStop");
+//
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean zoom(float initialDistance, float distance) {
+//                Gdx.app.log(TAG, "zoom");
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+//                Gdx.app.log(TAG, "pinch");
+//                return false;
+//            }
+//        });
+//        im.addProcessor(gd);
+//        im.addProcessor(this);
+//        Gdx.input.setInputProcessor(im);
+        w = Gdx.graphics.getWidth();
+        h = Gdx.graphics.getHeight();
         batch = new SpriteBatch();
 
-        texture = new Texture(Gdx.files.internal("jet.png"));
+        texture = new Texture(Gdx.files.internal("cat.png"));
         sprite = new Sprite(texture);
         sprite.setPosition(w / 2 - sprite.getWidth() / 2, h / 2 - sprite.getHeight() / 2);
     }
@@ -75,6 +87,18 @@ public class InputGame extends ApplicationAdapter implements InputProcessor {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        float accelX = Gdx.input.getAccelerometerX();
+        float accelY = Gdx.input.getAccelerometerY();
+        Gdx.app.log(TAG, "accelX = " + accelX);
+        Gdx.app.log(TAG, "accelY = " + accelY);
+
+//        float accelZ = Gdx.input.getAccelerometerZ();
+
+        sprite.setX((w / 2) - (sprite.getWidth() / 2) + accelY * 100);
+        sprite.setY((h / 2) - (sprite.getHeight() / 2) - accelX * 100);
+
 
 //        if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
 //            Gdx.app.log(TAG, "pressed");
@@ -90,7 +114,7 @@ public class InputGame extends ApplicationAdapter implements InputProcessor {
 //            else
 //                sprite.translateX(10.0f);
 //        }
-        batch.begin();
+
         sprite.draw(batch);
         batch.end();
     }
---
+++ core/src/com/kitowcy/SceneDemo.java
@@ -0,0 +1,64 @@
+package com.kitowcy;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class SceneDemo extends ApplicationAdapter {
+    MyActor myActor;
+
+    public class MyActor extends Actor {
+        Texture texture = new Texture(Gdx.files.internal("cat2.png"));
+
+        @Override
+        public void draw(Batch batch, float alpha) {
+            batch.draw(texture, 0, 0);
+            batch.setColor(Color.YELLOW);
+        }
+    }
+
+    private Stage stage;
+
+    @Override
+    public void create() {
+        stage = new Stage();
+
+//        ViewPort port =new ViewPort(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+//        stage.setViewport(port);
+
+        myActor = new MyActor();
+        stage.addActor(myActor);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        float accelX = Gdx.input.getAccelerometerX() * 10;
+        float accelY = Gdx.input.getAccelerometerY() * 100;
+
+
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+}
