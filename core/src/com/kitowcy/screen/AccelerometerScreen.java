8c2b26158a3dbd6ae4fcce2f13a7646bcd256650
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-01-30 12:16:54 +0100

    screens

---
+++ core/src/com/kitowcy/screen/AccelerometerScreen.java
@@ -0,0 +1,47 @@
+package com.kitowcy.screen;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Patryk Mieczkowski on 30.01.16.
+ */
+public class AccelerometerScreen implements Screen{
+
+    public AccelerometerScreen() {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
